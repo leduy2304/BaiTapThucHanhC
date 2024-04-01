ce75efd02d04737738cdf10806b9729a5de242f8
leduy2304 <152138238+leduy2304@users.noreply.github.com>
2024-04-01 08:35:50 +0700

    Add files via upload

---
+++ Bai1.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include <math.h>
+void inBoi7HaiChuSo() {
+	int i;
+	for (i = 10; i <= 98; i++) {
+		if (i % 7 == 0) {
+			printf("%d ", i);
+		}
+	}
+}
+
+int main() {
+	inBoi7HaiChuSo();
+	return 0;
+}
